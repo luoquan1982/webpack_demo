7a47128a483a1266c8c435b13b5fb6a8e91576a1
luoquan1982 <luo.quan1982@gmail.com>
2020-03-09 17:49:04 +0800

    暂存第一课

--- 03.打包样式资源/webpack.config.js
+++ 03.打包样式资源/webpack.config.js
@@ -21,18 +21,29 @@ module.exports = {
   },
   // loader的配置
   module: {
-    rulus: [
+    rules: [
       // 详细loader配置
+      // 不同文件必须配置不同loader处理
       {
         // 匹配那些文件
         test: /\.css$/,
         // 使用那些loader进行处理
         use: [
-            //use数组中loader执行顺序:从下到上依次执行
-            // 创建style标签,将js中的css样式资源插入进去,添加到head中生效
-            "style-loader", 
-            // 将css文件以字符串的形式变成一个commonjs的模块加载在js中,里面的内容是样式字符串
-            "css-loader"
+          // use数组中loader执行顺序:从右往左,从下到上 依次执行
+          // 创建style标签,将js中的css样式资源插入进去,添加到head中生效
+          "style-loader",
+          // 将css文件以字符串的形式变成一个commonjs的模块加载在js中,里面的内容是样式字符串
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader", 
+          "css-loader", 
+          // 将less文件编译成css文件
+          // 需要下载less-loader和less
+          "less-loader"
         ]
       }
     ]
